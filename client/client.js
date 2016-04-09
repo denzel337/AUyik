e0ea9b876ab89855cd9b92a38f3e41ca954e6c1e
Malik Inkatha <malikinkatha@Maliks-MacBook-Air.local>
2016-04-09 18:23:09 -0500

    Landing page remains after login

--- client/client.js
+++ client/client.js
@@ -2,7 +2,7 @@ Template.yaksList.helpers({
 	yaks: function() {
 		return Yaks.find({}, {sort : {score: -1}});
 	}
-})
+});
  var MAP_ZOOM = 15;
 
 Meteor.startup(function() {  
--- main.js
+++ main.js
@@ -2,15 +2,15 @@ Yaks = new Meteor.Collection('yaks');
 
 Comments = new Mongo.Collection("comments");
 
-Router.route('/', {name: 'accounts'})
+Router.route('/', {name: 'accounts'});
 
-Router.route('/wellness', {name: 'wellness'})
+Router.route('/wellness', {name: 'wellness'});
 
-Router.route('/wellnesssubmit', {name: 'wellnessSubmit'})
+Router.route('/wellnesssubmit', {name: 'wellnessSubmit'});
 
-Router.route('/wellnesslist', {name: 'wellnessList'})
+Router.route('/wellnesslist', {name: 'wellnessList'});
 
-Router.route('/yakslist', {name: 'yaksList'})
+Router.route('/yakslist', {name: 'yaksList'});
 
 Router.route('/submit', {name: 'yaksSubmit'});
 
